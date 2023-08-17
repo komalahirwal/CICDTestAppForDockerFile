a68ef7728d4bc8f2a3e65aa919b77deabaa0747b
Komal Ahirwal <komal.ahirwal@gmail.com>
2023-08-17 18:31:54 +0530

    Add project files.

---
+++ CICDTestAppForDocker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CICDTestAppForDocker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
